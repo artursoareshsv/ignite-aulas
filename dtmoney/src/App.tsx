d82830cd89b4bc722d4fa2df32ecf563bc0e48ed
Artur Vieira <artur.vieira@siemens.com>
2021-12-07 19:05:55 +0000

    creating useTransactions hooks.

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -4,7 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
-import { TransactionProvider } from "./TransactionContext";
+import { TransactionProvider } from "./components/hooks/useTransactions";
 
 ReactModal.setAppElement("#root");
 
