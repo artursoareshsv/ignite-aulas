ef596e1556ec946bceb1c47a3614dc4c924a5bb5
Artur Vieira <artur.vieira@siemens.com>
2021-12-13 17:58:38 +0000

    Sign in button style

---
+++ ignews/src/components/SignInButton/index.tsx
@@ -0,0 +1,22 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn
+        ? (
+            <button className={styles.singInButton} type="button">
+                <FaGithub color='#04d361' />
+                Artur Vieira
+                <FiX className={styles.closeIcon} color="#737380" />
+            </button>
+        )
+        : (
+            <button className={styles.singInButton} type="button">
+                <FaGithub color='#eba417' />
+                Sign in with Github
+            </button>
+        );
+}
