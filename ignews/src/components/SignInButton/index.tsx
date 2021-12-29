7c6d05ad7ff7ea98cf9bf38c45b2af323aa2dbe5
Artur Vieira <artur.vieira@siemens.com>
2021-12-29 16:18:52 +0000

    Sign in and out with next auth

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,20 +1,22 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data } = useSession();
+    console.log(data);
 
-    return isUserLoggedIn
+    return data
         ? (
-            <button className={styles.singInButton} type="button">
+            <button className={styles.singInButton} type="button" onClick={() => signOut()}>
                 <FaGithub color='#04d361' />
-                Artur Vieira
+                {data.user.name}
                 <FiX className={styles.closeIcon} color="#737380" />
             </button>
         )
         : (
-            <button className={styles.singInButton} type="button">
+            <button className={styles.singInButton} type="button" onClick={() => signIn('github')}>
                 <FaGithub color='#eba417' />
                 Sign in with Github
             </button>
