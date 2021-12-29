b79e1923d5f78c2dfba9d70d988e7b4601c60f33
Artur Vieira <artur.vieira@siemens.com>
2021-12-29 15:35:34 +0000

    Creating api route

--- ignews/src/services/stripe.ts
+++ ignews/src/services/stripe.ts
@@ -1,10 +1,10 @@
 import { Stripe } from 'stripe';
-import * as pack from '../../package.json';
+import { version } from '../../package.json';
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
 	apiVersion: '2020-08-27',
 	appInfo: {
 		name: 'ignews',
-		version: pack.version,
+		version,
 	},
 });
