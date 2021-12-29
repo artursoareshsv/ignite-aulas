a1efbb0349774b986b13561f0a5b300a17d63ada
Artur Vieira <artur.vieira@siemens.com>
2021-12-29 17:23:10 +0000

    Avoid unique error

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
 	providers: [
@@ -8,4 +10,26 @@ export default NextAuth({
 			clientSecret: process.env.GITHUB_SECRET,
 		}),
 	],
+	secret: process.env.SECRET,
+	callbacks: {
+		signIn: async ({ user }) => {
+			try {
+				await fauna.query(
+					query.If(
+						// condition
+						query.Not(query.Exists(query.Match(query.Index('user_by_email'), query.Casefold(user.email)))),
+						// then
+						query.Create(query.Collection('users'), { data: { email: user.email } }),
+						// else
+						query.Get(query.Match(query.Index('user_by_email'), query.Casefold(user.email)))
+					)
+				);
+
+				return true;
+			} catch (error) {
+				console.error(error);
+				return false;
+			}
+		},
+	},
 });
