a424ac8e53088cc865956d73aa74fa46e4f0e6cb
Artur Vieira <artur.vieira@siemens.com>
2021-12-30 16:22:34 +0000

    Updating subscription

--- ignews/src/pages/api/_lib/manageSubscription.ts
+++ ignews/src/pages/api/_lib/manageSubscription.ts
@@ -2,7 +2,7 @@ import { query } from 'faunadb';
 import { fauna } from '../../../services/fauna';
 import { stripe } from '../../../services/stripe';
 
-export async function saveSubscription(subscriptionId: string, costumerId: string) {
+export async function saveSubscription(subscriptionId: string, costumerId: string, createAction: boolean): Promise<void> {
 	const userRef = await fauna.query(query.Select('ref', query.Get(query.Match(query.Index('user_by_stripe_costumer_id'), costumerId))));
 	const subscription = await stripe.subscriptions.retrieve(subscriptionId);
 
@@ -13,5 +13,13 @@ export async function saveSubscription(subscriptionId: string, costumerId: strin
 		price_id: subscription.items.data[0].price.id,
 	};
 
-	await fauna.query(query.Create(query.Collection('subscriptions'), { data: subscriptionData }));
+	if (createAction) {
+		await fauna.query(query.Create(query.Collection('subscriptions'), { data: subscriptionData }));
+	} else {
+		await fauna.query(
+			query.Replace(query.Select('ref', query.Get(query.Match(query.Index('subscription_by_id'), subscriptionId))), {
+				data: subscriptionData,
+			})
+		);
+	}
 }
