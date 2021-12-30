d70d63566154fe2316e23d9462682b0432cd3db0
Artur Vieira <artur.vieira@siemens.com>
2021-12-30 14:57:59 +0000

    Avoid doubles on stripe costumer.

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,5 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
-import { stripe } from '../../services/stripe';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -9,13 +10,22 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const { data } = useSession();
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (!data) {
             signIn('github');
             return;
         }
 
+        try {
+            const response = await api.post('/subscribe');
 
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+            stripe.redirectToCheckout({ sessionId });
+        } catch (error) {
+            alert(error.message);
+        }
     }
 
     return (
